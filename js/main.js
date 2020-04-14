505c75919ead0257ad926c3fcbc955d60f72157d
Alex Notov <alex@zealoushacker.com>
2020-04-13 21:03:52 -0600

    Split out all js into its own file

---
+++ js/main.js
@@ -0,0 +1,193 @@
+var stateInfoTip = d3.tip()
+  .attr('class', 'd3-tip b-r-6')
+  .offset([0, 0])
+  .direction(function(d) {
+    states = ["Washington", "Oregon","Idaho", "Montana", "North Dakota", "Minnesota", "Wisconsin", "Michigan", "New Hampshire", "Maine", "Vermont"]
+    if (states.includes(d.properties.STATE)) {
+      return "s";
+    } else {
+      return "n"
+    }
+  })
+  .html(function(d) {
+    return '<div class="has-text-weight-bold m-b-2">' + d.properties.STATE + '</div>' +
+      '<div class="has-text-danger m-b-1 is-size-7">' + getNumberOfClaimsForState(d.properties.STATE).toLocaleString() + '</div>' +
+      '<div class="has-text-danger is-size-7">Unemployed</div>';
+});
+
+var closeModal = function(target) {
+  target.style.opacity = 0; 
+  target.style.display = 'none';
+}
+
+var showCityPersonModal = function(d) { 
+  d3.select('#city-person-modal')
+    .selectAll('div')
+    .remove();
+
+  d3.select('#city-person-modal')
+    .datum(d.fields)
+    .insert('div', ':first-child')
+    .html(function(person) { 
+      return '<div class="card has-background-black">' +
+          '<div class="card-image">' +
+            '<figure class="image is-4by3">' +
+              '<img src="' + person.photo[0].url + '" alt="Placeholder image" style="object-fit: cover">' +
+            '</figure>' +
+          '</div>' +
+          '<div class="card-content p-3">' +
+            '<p class="title is-size-8 has-text-white m-b-2">' + person.name + '</p>' +
+            '<p class="is-size-9 has-text-grey-light">' +
+              person.title +
+              '<br>' +
+              person.company +
+              '<br>' +
+              person.locations[0] +
+            '</p>' +
+            '<p class="is-size-9 m-t-2 has-text-grey-light">' +
+              person.bio +
+            '</p>' +
+            '<p class="is-size-9 m-t-3 has-text-grey-light">' +
+              '<a href="mailto:' + person.email + '" alt="Email" class="m-r-2"><img src=""/><i class="mdi mdi-email"></i></a>' +
+              '<a href="' + person.resume[0].url + '" alt="Resume" class="m-r-2"><img src=""/><i class="mdi mdi-file-document"></i></a>' +
+              '<a href="' + person.linkedin_url + '" alt="LinkedIn"><img src=""/><img src=""/><i class="mdi mdi-linkedin"></i></a>' +
+            '</p>' +
+            '<p class="m-t-4">' +
+              '<a href="javascript:showCityPeopleModal(\'' + person.locations[0] + '\')" class="button is-link is-fullwidth is-size-9 is-padded has-text-weight-bold">View More People</a>' +
+            '</p>' +
+          '</div>' +
+        '</div>';
+    });
+  d3.select('#city-person-modal').attr('style', 'opacity: 1');
+};
+
+var hideCityPersonModal = function() {
+  d3.select('#city-person-modal').attr('style', 'opacity: 0');
+};
+
+var showCityPeopleModal = function(location) {
+  d3.select('#city-people-modal')
+    .selectAll('div')
+    .remove();
+
+  d3.select('#city-people-modal')
+    .selectAll('div')
+    .data(_mostRecentLayoffsByLocation.records.filter(function(r) { return r.fields.locations[0] == location; }))
+    .enter()
+    .insert('div', ':first-child')
+    .attr('class', 'person-in-list')
+    .html(function(d) { 
+      return '<div><img src="' + d.fields.photo[0].url + '" class="profile-image"/></div>' +
+        '<div>' + d.fields.name + '</div>' +
+        '<div>' + d.fields.title + '</div>'
+    })
+    .on('click', function(d) { showCityPersonModal(d); });
+  d3.select('#city-people-modal').attr('style', 'opacity: 1');
+};
+
+var getColorForNumberOfClaims = function(stateName) {
+  var stateClaims = getNumberOfClaimsForState(stateName); 
+  if (!stateClaims) {
+    return '#ccc';
+  } else {
+    return _claimsColorScale(stateClaims);
+  }
+};
+
+var getNumberOfClaimsForState = function(stateName) {
+  return _stateClaims[stateName]; 
+};
+
+var _claimsColorScale;
+
+var _states, _claims, _stateClaims = {}, _mostRecentLayoffsByLocation;
+
+var drawMostRecentLayoffsByLocation = function(layoffs) {
+  var circleGroup = svg.selectAll('circle')
+    .data(layoffs.records)
+    .enter()
+    .append('g')
+    .attr('transform', function(d) {
+      return 'translate('+ projection([d.fields.lon[0], d.fields.lat[0]])+')';
+                })
+    .attr('class', 'city');
+
+  circleGroup
+    .append('circle')
+    .attr('r', 5)
+    .on('click', function(d, i) { 
+      showCityPersonModal(d); 
+      closeModal(document.querySelector('#city-people-modal'));
+    });
+};
+
+var dataReady = function(error, states, claims, layoffs) {
+  if (error) throw error;
+  _states = states;
+  _claims = claims;
+  _mostRecentLayoffsByLocation = layoffs;
+
+  var minClaims = claims.records[claims.records.length - 1].fields.claims;
+  var maxClaims = claims.records[0].fields.claims;
+
+  _claimsColorScale = d3.scaleQuantize()
+    .domain([minClaims, maxClaims])
+    .range(['#280406', '#840e15', '#b3121c', '#d11621', '#e8212d']);
+
+
+  // build up the state claim numbers dictionary for easy retrieval later
+  _claims.records.forEach(function(claim) {
+    _stateClaims[claim.fields.state] = claim.fields.claims;
+  });
+
+  renderMap(_states, _claims);
+  drawMostRecentLayoffsByLocation(layoffs);
+}
+
+var renderMap = function(states, claims) {
+  svg.selectAll('*').remove(); // clear for re-render
+  
+  svg.selectAll('.states')
+    .data(topojson.feature(states, states.objects.states).features)
+    .enter()
+    .append('path')
+    .attr('class', 'state')
+    .style('fill', function (st) { 
+      return getColorForNumberOfClaims(st.properties.STATE) 
+    })
+    .attr('d', path)
+    .on('mouseover', stateInfoTip.show)
+    .on('mouseout', stateInfoTip.hide)
+    .on('click', hideCityPersonModal);
+}
+
+var svg = d3.select('#container').append('svg')
+  .attr('xmlns', 'http://www.w3.org/2000/svg')
+  .attr('version', '1.1')
+  .attr('viewBox', '0 0 900 500')
+  .attr('preserveAspectRatio', 'xMidYMid meet')
+  .classed('svg-content', true);
+
+svg.call(stateInfoTip);
+var projection = d3.geoAlbersUsa();
+var path = d3.geoPath().projection(projection);
+
+var claimsRequest = d3.json('https://api.airtable.com/v0/appQWZIOAEl6UiPa3/ui_claims?view=Grid%20view')
+  .header('Authorization', 'Bearer keyLVEYQelX6aEgaR');
+
+var mostRecentLayoffsByLocationRequest = d3.json('https://api.airtable.com/v0/appQWZIOAEl6UiPa3/people?view=map_view')
+  .header('Authorization', 'Bearer keyLVEYQelX6aEgaR');
+
+d3.queue()
+  .defer(d3.json, 'data/us-states-simplified.json')
+  .defer(claimsRequest.get)
+  .defer(mostRecentLayoffsByLocationRequest.get)
+  .await(dataReady);
+
+document.querySelectorAll('.close').forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    closeModal(e.target.parentNode);
+  }, false);
+}); 
