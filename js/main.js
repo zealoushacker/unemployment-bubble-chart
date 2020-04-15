6dde4d2643288423327e633b8374aa0603c9ce9c
Alex Notov <alex@zealoushacker.com>
2020-04-15 16:35:31 -0600

    Set absolute url for links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+var getAbsoluteUrl = (function() {
+  var a;
+
+  return function(url) {
+    if(!a) a = document.createElement('a');
+    a.href = url;
+
+    return a.href;
+  };
+})();
+
 var stateInfoTip = d3.tip()
   .attr('class', 'd3-tip b-r-6 shadow')
   .offset([0, 0])
@@ -46,8 +57,8 @@ var showPersonModal = function(d) {
             '</p>' +
             '<p class="is-size-7 m-t-3 has-text-grey-light">' +
               '<a href="mailto:' + person.email + '" alt="Email" class="m-r-2"><img src=""/><i class="mdi mdi-email"></i></a>' +
-              '<a href="' + person.resume[0].url + '" alt="Resume" target="_blank" class="m-r-2"><img src=""/><i class="mdi mdi-file-document"></i></a>' +
-              '<a href="' + person.linkedin_url + '" alt="LinkedIn" target="_blank"><img src=""/><img src=""/><i class="mdi mdi-linkedin"></i></a>' +
+              '<a href="' + getAbsoluteUrl(person.resume[0].url) + '" alt="Resume" target="_blank" class="m-r-2"><img src=""/><i class="mdi mdi-file-document"></i></a>' +
+              '<a href="' + getAbsoluteUrl(person.linkedin_url) + '" alt="LinkedIn" target="_blank"><img src=""/><img src=""/><i class="mdi mdi-linkedin"></i></a>' +
             '</p>' +
             '<p class="m-t-4">' +
               '<a href="javascript:showListModal(\'' + person.locations[0] + '\')" class="button is-link is-size-8 is-padded has-text-weight-bold">View More People</a>' +
